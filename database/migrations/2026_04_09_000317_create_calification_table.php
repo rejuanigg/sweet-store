2c04c3715d20e0eaf2b9c554755a2792ffc91bde
rejuanigg <riosjuan32223@gmail.com>
2026-05-19 10:16:47 -0300

    fix:change score int

--- database/migrations/2026_04_09_000317_create_calification_table.php
+++ database/migrations/2026_04_09_000317_create_calification_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table
             ->foreignId('user_id')
             ->constrained('users');
-            $table->decimal('score', 10, 2);
+            $table->unsignedTinyInteger('score');
             $table->timestamps();
         });
     }
